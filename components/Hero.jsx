1e027cb68c832fedb10f443d25db5d71dfa582b9
Emeka Manuel <apple@Manuels-MacBook-Pro.local>
2023-03-28 01:59:57 +0100

    made changes to background

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,7 +4,7 @@ const Hero = ({heading, message}) => {
   return (
     <div className='flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-img'>
         {/* ///overlay */}
-        <div className='absolute left-0 top-0 right-0 bottom-0 bg-black/80 z-[2]'/>
+        <div className='absolute left-0 top-0 right-0 bottom-0 bg-black/60 z-[2]'/>
         <div className='p-5 text-white z-[2] mt-[-10rem]'>
             <h2 className='text-5xl font-bold'>{heading}</h2>
             <h2 className='py-5 text-xl'>{message}</h2>
